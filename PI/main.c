2ac0fb26c412a6a17aa35b59bc5961aa3937afcc
lmbaeza <lmbaeza@unal.edu.co>
2019-04-17 13:50:44 -0500

    Actualizar algoritmo PI - WAIT

--- PI/main.c
+++ PI/main.c
@@ -1,7 +1,8 @@
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/types.h>
-#include<sys/wait.h> 
+#include <sys/wait.h> 
 #include <unistd.h>
 #include <math.h>
 
@@ -15,11 +16,12 @@ int save(double ap) {
     exit(-1);
   }
 
-  fprintf(fp, "%f", ap);
+  fprintf(fp, "%.10f", ap);
   fclose(fp);
 
   return 0;
 }
+
 
 int readf(double *ap) {
   FILE *fp;
@@ -29,23 +31,24 @@ int readf(double *ap) {
     exit(-1);
   }
 
-  fscanf(fp, "%lf", ap);
+  fscanf(fp, "%le", ap);
   fclose(fp);
   return EXIT_SUCCESS;
 }
 
-double pi(int start, int end) {
+
+double pi(double start, double end) {
   double out = 0.0;
   double signo = 1;
   double x = 0.0;
 
-  for(unsigned long i = start; i < end; i+=2) {
-    x = (double)(1.0/(double)i);
+  for(double i = start; i < end; i+=2.0) {
+    x = 4.0/i;
     x = x * signo;
     signo *= -1;
     out += x;
   }
-  return out * 4.0;
+  return out;
 }
 
 
@@ -54,30 +57,36 @@ int main(void) {
   pid_t p;
   p = fork();
 
-  double aux = 0;
-  double result = 0;
+  double aux = 0.0;
+  double result = 0.0;
 
-  unsigned long number = 200000000;
+  double number = 2e09;
+  //3.14159265358
 
   if(p < 0) {
     perror("Error fork(...)\n");
     exit(-1);
   } else if(p == 0){
-    result = pi(1, number/2);
+    result = pi(1.0, (number/2));
 
     save(result);
-    printf("Hola soy Hijo %f\n", result);
+    printf("Hola soy Hijo  %f\n", result);
 
   } else{
-    wait(NULL);
-    
-    readf(&aux);
-    result += pi(number/2, number);
 
+    result += pi((double)(number/2), (double)number);
+
+    wait(NULL);
+    readf(&aux);
+    
     result += aux;
     printf("Hola soy Padre %.10f\n", result);
-  }
 
+  }
   
   return 0;
-} 
+}
+//Out[]
+//Hola soy Hijo  3.141593
+//Hola soy Padre 3.1415926526
+
